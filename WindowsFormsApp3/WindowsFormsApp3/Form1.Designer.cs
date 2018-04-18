68bbc3a49ae06920f97b4cff163c5e82b311fbf3
Thomas Stephenson <34628271+stevot1995@users.noreply.github.com>
2018-04-18 13:45:55 +0100

    Hid the media play from the executed app

--- WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -74,8 +74,9 @@
             this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(9, 12);
             this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
             this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
-            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(271, 49);
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(215, 96);
             this.axWindowsMediaPlayer2.TabIndex = 1;
+            this.axWindowsMediaPlayer2.Visible = false;
             // 
             // listBox1
             // 
@@ -88,7 +89,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(705, 40);
+            this.button1.Location = new System.Drawing.Point(369, 690);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 106);
             this.button1.TabIndex = 4;

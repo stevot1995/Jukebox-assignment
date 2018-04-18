d8d611a2410c2ec37d2ccf42a931b9f0ee07ec18
Thomas Stephenson <34628271+stevot1995@users.noreply.github.com>
2018-04-18 13:53:20 +0100

    Adjusted the GUI

--- WindowsFormsApp3/WindowsFormsApp3/Form3.Designer.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form3.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(76, 676);
+            this.button1.Location = new System.Drawing.Point(353, 676);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(451, 233);
             this.button1.TabIndex = 0;
@@ -67,7 +67,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(76, 937);
+            this.button2.Location = new System.Drawing.Point(353, 938);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(451, 168);
             this.button2.TabIndex = 2;
